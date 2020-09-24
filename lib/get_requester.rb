758f5af1be045fb33401e0e305d458512448fdc4
Dasha Lary <dk2800@nyu.edu>
2020-09-24 20:17:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,7 @@ class GetRequester
   
   def initialize(url)
   @url = url 
-  @url = 
+  @url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 end 
   
   def get_response_body
